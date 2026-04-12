01ff94b22017e01022ccf288b1ddf04a5c5124f6
Anton <anton.krivozubov@mail.ru>
2026-04-12 18:34:42 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,19 +3,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProd = mode === "production";
+// export default defineConfig(({ mode }) => {
+//   const isProd = mode === "production";
 
-  return {
-    base: isProd ? "/cat-pinterest/" : "/",
-    plugins: [react()],
-    server: {
-      open: true,
+//   return {
+//     base: isProd ? "/cat-pinterest/" : "/",
+//     plugins: [react()],
+//     server: {
+//       open: true,
+//     },
+//     resolve: {
+//       alias: {
+//         "@": fileURLToPath(new URL("./src", import.meta.url)),
+//       },
+//     },
+//   };
+// });
+
+export default defineConfig({
+  base: "/cat-pinterest/",
+  plugins: [react()],
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    },
-  };
+  },
 });
