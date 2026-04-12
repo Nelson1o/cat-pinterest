542149b64e7571e3e272b07ec202515d122482df
Anton <anton.krivozubov@mail.ru>
2026-04-12 18:30:14 +0300

    fix: fix router path

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,13 +2,18 @@ import { createBrowserRouter, redirect } from "react-router";
 
 import { HomePage } from "@/pages";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      loader: () => redirect("/gallery"),
+    },
+    {
+      path: "/:type",
+      element: <HomePage />,
+    },
+  ],
   {
-    path: "/",
-    loader: () => redirect("/gallery"),
-  },
-  {
-    path: "/:type",
-    element: <HomePage />,
-  },
-]);
+    basename: "/cat-pinterest",
+  }
+);
